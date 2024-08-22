80b13c7dff35afefae1fb7414c3dd0e0222d7003
Thanutham Baikwang <thanutham.j@gmail.com>
2024-08-22 14:16:59 +0700

    feat: add answer to excercises

--- ex-6.js
+++ ex-6.js
@@ -1,10 +1,12 @@
-const student = {
+const Student = [{
   name: "John Doe",
+  age: 18,
   grade: 12,
   subjects: ["Math", "Science", "History"],
-};
+}
+];
 
-console.log("Student Name: " + stdent.name);
-console.log("Student Age: " + student.age);
-console.log("Student Grade: " + student.gade);
-console.log("Student Subjects: " + student.subjects);
+console.log("Student Name: " + Student[0].name);
+console.log("Student Age: " + Student[0].age);
+console.log("Student Grade: " + Student[0].grade);
+console.log("Student Subjects: " + Student[0].subjects);
